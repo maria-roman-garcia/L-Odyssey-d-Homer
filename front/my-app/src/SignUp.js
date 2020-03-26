b55d2d4e4971a34fb475f6c1ee00a5111a72acde
MariaRomanDelonia <maria.roman@delonia.es>
2020-03-26 16:04:15 +0100

    form en progreso

--- front/my-app/src/SignUp.js
+++ front/my-app/src/SignUp.js
@@ -4,18 +4,51 @@ class SignUp extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { email: 'test@test.com' };
+        this.state = { 
+            email: 'test@test.com',
+            password: "monPassw0rd",
+            passwordBis: "monPassw0rd",
+            name: "James",
+            lastname: "Bond"
+         };
 
         this.updateEmailField = this.updateEmailField.bind(this);
+        this.updatePasswordField = this.updatePasswordField.bind(this);
+        this.updatepasswordBisField = this.updatepasswordBisField.bind(this);
+        this.updateNameField = this.updateNameField.bind(this);
+        this.updateLastNameField = this.updateLastNameField.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     updateEmailField(event) {
-        this.setState({ email: event.target.value });
+        this.setState({ 
+            email: event.target.value,
+         });
+    }
+    updatePasswordField(event){
+        this.setState({
+            password: event.target.value
+        });
+    }
+    updatepasswordBisField(event) {
+        this.setState({
+            passwordBis: event.target.value
+        });
+    }
+    updateNameField(event){
+        this.setState({
+            name: event.target.value
+        })
+    }
+    updateLastNameField(event) {
+        this.setState({
+            lastname: event.target.value
+        })
     }
 
     handleSubmit(event) {
-        alert('An email was submitted: ' + this.state.email);
+        alert(JSON.stringify(this.state, 1, 1));
+        console.log(JSON.stringify(this.state, 1, 1))
         event.preventDefault();
     }
 
@@ -23,8 +56,12 @@ class SignUp extends Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    <h1>Bienvenid@ , {this.state.email}</h1>
+                    <h1>{JSON.stringify(this.state,1,1)}</h1>
                     <input type="email" name="email" value={this.state.email} onChange={this.updateEmailField} />
+                    <input type="password" name="password" value={this.state.password} onChange={this.updatePasswordField} />
+                    <input type="password" name="passwordBis" value={this.state.password} onChange={this.updatepasswordBisField} />
+                    <input type="text" name="name" value={this.state.name} onChange={this.updateNameField} />
+                    <input type="text" name="lastName" value={this.state.lastname} onChange={this.updateLastNameField} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
